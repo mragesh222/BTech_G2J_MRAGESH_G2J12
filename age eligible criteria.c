202fdd8169000d1852dad6972507ea1ec9478a28
mragesh222 <25116cn138@glbitm.ac.in>
2026-02-25 11:54:52 +0530

    Create age eligible criteria.c

---
+++ age eligible criteria.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("enter your age");
+    scanf("%d",&a);
+    if(a>=18)
+    {
+        printf("user is eligible");
+    }else
+    {
+        printf("user is not eligible");
+    }
+ 
+ return 0;
+}
